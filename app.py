73b456a8e8f3c455bd143e181e4d5111795d8690
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-10-23 19:29:24 +0000

    designing the website with css

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def register():
         register = mongo.db.register
         register.insert_one(request.form.to_dict())
         print(register)
-        return redirect('/get_recipe')
+        return redirect('/get_recipe','register_id=register._id')
     return render_template('register.html')
 
 @app.route('/login', methods=["GET","POST"])
@@ -31,25 +31,24 @@ def login():
         if login_user:
             if(form["password"] == login_user["password"]): # if password correct
                 session['username'] = login_user["username"]
-                return redirect(url_for('get_recipe', register_id = login_user["_id"]))
+                return redirect(url_for('get_recipe',register_id = login_user["_id"]))
             else: # and if password is not correct
-                message = "Incorrect password"
+               flash("Incorrect password") 
         else:# if not exist
-            message = "User does not exist"
-    return render_template('login.html', message=message)
+            flash("User does not exist")
+            return redirect(url_for('register'))
+    return render_template('login.html')
             
             
+@app.route('/delete_register/<register_id>',methods=["GET","POST"] )
+def delete_register(register_id):
+    mongo.db.register.remove({'_id':ObjectId(register_id)})
+    return redirect(url_for('login'))
 
-@app.route('/edit_register/<register_id>', methods=['POST','GET'])
-def edit_register(register_id):
-    register=mongo.db.register.find_one({'_id':ObjectId(register_id)})
-    print(register)
-    return render_template('edit_register.html',register=register)
-
-@app.route('/edit_recipes/<recipes_id>',methods=['POST','GET'])
-def edit_recipes(recipes_id):
+@app.route('/edit_recipes/<recipes_id>/<register_id>',methods=['POST','GET'])
+def edit_recipes(recipes_id,register_id):
     recipes=mongo.db.recipes.find_one({'_id':ObjectId(recipes_id)})
-    return render_template('edit_recipes.html',recipes=recipes)
+    return render_template('edit_recipes.html',recipes=recipes,register = mongo.db.register.find_one({'_id':ObjectId(register_id)}))
     
 @app.route('/update_recipes/<recipes_id>',methods=['POST'])
 def update_recipes(recipes_id):
@@ -63,15 +62,28 @@ def update_recipes(recipes_id):
                     "cooking":request.form.get('cooking')
     } )
     return redirect(url_for('get_recipe'))
- 
-@app.route('/get_recipe',methods=['POST','GET'])
-def get_recipe():
-    register = mongo.db.register.find()
+    
+@app.route('/edit_register/<register_id>', methods=['POST','GET'])
+def edit_register(register_id):
+    register=mongo.db.register.find_one({'_id':ObjectId(register_id)})
     print(register)
+    return render_template('edit_register.html',register=register)
+@app.route('/update_register/<register_id>', methods=['GET','POST'])
+def update_register(register_id):
+    register = mongo.db.register
+    register.update({'_id':ObjectId(register_id)},{"username":request.form.get('username'),
+               "email":request.form.get('email'),
+               "password":request.form.get('password')})
+    return redirect('login',)
+    
+@app.route('/get_recipe/<register_id>',methods=['POST','GET'])
+def get_recipe(register_id):
+    register = mongo.db.register.find_one({'_id':ObjectId(register_id)})
     return render_template('get_recipe.html',recipes=mongo.db.recipes.find(), register=register)
-@app.route('/add_recipes',methods=['POST','GET'])
-def add_recipes():
-    return render_template('add_recipes.html',recipes=mongo.db.recipes.find(), register = mongo.db.register.find_one() )
+@app.route('/add_recipes/<register_id>',methods=['POST','GET'])
+def add_recipes(register_id):
+    print(register)
+    return render_template('add_recipes.html',recipes=mongo.db.recipes.find(), register = mongo.db.register.find_one({'_id':ObjectId(register_id)}))
     
 @app.route('/insert_recipes', methods=["POST"])
 def insert_recipes():
@@ -86,6 +98,7 @@ def insert_recipes():
     }  
     recipes.insert_one(recipes_doc)
     print(recipes)
+    
     return redirect('/get_recipe')
 
 @app.route('/show_recipes/<recipes_id>',methods=['POST','GET'])
@@ -93,6 +106,11 @@ def show_recipes(recipes_id):
     recipes=mongo.db.recipes.find({'_id':ObjectId(recipes_id)})
     return render_template('show_recipes.html',recipes=recipes, register = mongo.db.register.find_one())
     
+@app.route('/search_recipes/<register_id>', methods=['POST','GET'])
+def search_recipes(register_id):
+    query = { "name": { "$regex": request.form.get('search') } }
+    recipes = mongo.db.recipes.find_one(query)
+    return render_template('search_recipes.html',register = mongo.db.register.find_one({'_id':ObjectId(register_id)}), recipes= recipes )
 
 @app.route('/vote/<recipes_id>', methods=["POST"])
 def upvote(recipes_id):
