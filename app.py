c60ed5e4e1e4ad6fedb0d0d79e2185b40c82e359
Ramanathan Annes <ramanathanannes47@gmail.com>
2018-10-31 20:25:28 +0000

    edit reipe is working now but I have to test more

--- app.py
+++ app.py
@@ -69,26 +69,25 @@ def delete_register(register_id):
 @app.route('/edit_recipes/<recipes_id>/<register_id>',methods=['POST','GET'])           # edit the recipe
 def edit_recipes(recipes_id,register_id):
     recipes=mongo.db.recipes.find_one({'_id':ObjectId(recipes_id)})
+    print(request.form.get('recipe_name'))
     return render_template('edit_recipes.html',recipes=recipes,register = mongo.db.register.find_one({'_id':ObjectId(register_id)}))
     
 @app.route('/update_recipes/<recipes_id>/<register_id>',methods=['GET','POST'])
 def update_recipes(recipes_id,register_id):
     # if request.method == "POST":
     recipes=mongo.db.recipes
-    recipes.update({'_id':ObjectId(recipes_id)},  
-    {"name":request.form.get("name"),
+    update = recipes.update({'_id':ObjectId(recipes_id)},  
+    {          "name":request.form.get("name"),
                "ingredients":request.form.get("ingredients"),
                 "steps":request.form.get("steps"),
                 "imageURL":request.form.get("imageURL"),
                 "creditTo":request.form.get("credit"),
                 "preparation":request.form.get("preparation"),
                 "cooking":request.form.get("cooking")})
-    print(recipes.id)
-    print(request.form)
     print(recipes)
-    sys.exit('error')
-    return redirect(url_for('login'))
-    #return redirect(url_for('show_recipes', recipes_id=recipes_id,register_id=register_id))
+    print(request.form.get('recipe_name'))
+    print(update)
+    return redirect(url_for('show_recipes', recipes_id=recipes_id,register_id=register_id))
     
 @app.route('/edit_register/<register_id>', methods=['POST','GET'])
 def edit_register(register_id):
@@ -102,6 +101,7 @@ def update_register(register_id):
     register.update({'_id':ObjectId(register_id)},{"username":request.form.get('username'),
                "email":request.form.get('email'),
                "password":request.form.get('password')})
+    print(request.form.get('email'))
     return redirect('login')
 
 
@@ -114,6 +114,7 @@ def get_recipe(register_id):
 @app.route('/add_recipes/<register_id>',methods=['POST','GET'])
 def add_recipes(register_id):
     print(register)
+    print(request.form.get('recipe_name'))
     return render_template('add_recipes.html',recipes=mongo.db.recipes.find(), register = mongo.db.register.find_one({'_id':ObjectId(register_id)}))
     
 @app.route('/insert_recipes/<register_id>', methods=["POST"])
